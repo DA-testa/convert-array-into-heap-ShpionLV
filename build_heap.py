842d09781594003ebb5a21d542790a1947b30f77
ShpionLV <60115605+ShpionLV@users.noreply.github.com>
2023-03-19 17:31:29 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,56 @@
-# python3
+from typing import List, Tuple
 
 
-def build_heap(data):
+def swap(data: List[int], i: int, j: int) -> None:
+    data[i], data[j] = data[j], data[i]
+
+
+def heapify(parent_index: int, data: List[int], swaps: List[Tuple[int, int]]) -> None:
+    left_child_index = parent_index * 2 + 1
+    right_child_index = parent_index * 2 + 2
+    smallest_child_index = parent_index
+
+    if left_child_index < len(data) and data[left_child_index] < data[smallest_child_index]:
+        smallest_child_index = left_child_index
+
+    if right_child_index < len(data) and data[right_child_index] < data[smallest_child_index]:
+        smallest_child_index = right_child_index
+
+    if smallest_child_index != parent_index:
+        swap(data, smallest_child_index, parent_index)
+        swaps.append((parent_index, smallest_child_index))
+        heapify(smallest_child_index, data, swaps)
+
+
+def build_heap(data: List[int]) -> List[Tuple[int, int]]:
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    last_non_leaf_index = (len(data) - 2) // 2
 
+    for i in range(last_non_leaf_index, -1, -1):
+        heapify(i, data, swaps)
 
     return swaps
 
 
-def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+def main() -> None:
+    test_type = input("Choose a test type (I or F): ").lower()
 
+    if test_type == "i":
+        n = int(input("Node amount: "))
+        data = list(map(int, input("Nodes: ").split()))
+    elif test_type == "f":
+        filename = input("Enter file name: ")
+        with open(f"tests/{filename}", 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    else:
+        raise ValueError("Invalid input type. Expected I or F.")
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
