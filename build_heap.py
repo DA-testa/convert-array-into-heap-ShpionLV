d445cbb4716623fc240c031025e04b521c5c1587
ShpionLV <60115605+ShpionLV@users.noreply.github.com>
2023-03-19 17:35:05 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,53 +1,76 @@
-from typing import List, Tuple
+# python3
+from math import log2, ceil, floor
 
 
-def swap(data: List[int], i: int, j: int) -> None:
-    data[i], data[j] = data[j], data[i]
+def swap(data, node_one_index, node_two_index):
+    temp = data[node_one_index]
+    data[node_one_index] = data[node_two_index]
+    data[node_two_index] = temp
+
+    return data
+
+def find_last_nonleaf_node(n):
+    full_row_amount = floor(log2(n+1))
+    last_row_node_amount = n - (2**full_row_amount - 1)
+    used_parent_nodes = ceil(last_row_node_amount / 2)
+    last_full_row_node_index = 2**(full_row_amount-1)-2
+    index = last_full_row_node_index + used_parent_nodes
+
+    return index
 
 
-def heapify(parent_index: int, data: List[int], swaps: List[Tuple[int, int]]) -> None:
-    left_child_index = parent_index * 2 + 1
-    right_child_index = parent_index * 2 + 2
-    smallest_child_index = parent_index
+def heapify(parent_index: int, data: list, swaps: list):
+    children_one_index = parent_index*2 + 1
 
-    if left_child_index < len(data) and data[left_child_index] < data[smallest_child_index]:
-        smallest_child_index = left_child_index
+    if children_one_index >= len(data):
+        return data
 
-    if right_child_index < len(data) and data[right_child_index] < data[smallest_child_index]:
-        smallest_child_index = right_child_index
+    children_two_index = parent_index*2 + 2
+    smaller_child_index = children_one_index
 
-    if smallest_child_index != parent_index:
-        swap(data, smallest_child_index, parent_index)
-        swaps.append((parent_index, smallest_child_index))
-        heapify(smallest_child_index, data, swaps)
+    if children_two_index < len(data):
+        if data[children_two_index] < data[children_one_index]:
+            smaller_child_index = children_two_index
+
+    if data[smaller_child_index] < data[parent_index]:
+        swap(data, parent_index, smaller_child_index)
+        swaps.append([parent_index, smaller_child_index])
+
+        heapify(smaller_child_index, data, swaps)
+
+    return data
 
 
-def build_heap(data: List[int]) -> List[Tuple[int, int]]:
+def build_heap(data, n):
     swaps = []
-    last_non_leaf_index = (len(data) - 2) // 2
 
-    for i in range(last_non_leaf_index, -1, -1):
+    last_nonleaf_node_index = find_last_nonleaf_node(n)
+    for i in range(last_nonleaf_node_index, -1, -1):
         heapify(i, data, swaps)
 
     return swaps
 
 
-def main() -> None:
+def main():
+
+
     test_type = input("Choose a test type (I or F): ").lower()
 
-    if test_type == "i":
+    if test_type.find("i") != -1:
         n = int(input("Node amount: "))
         data = list(map(int, input("Nodes: ").split()))
-    elif test_type == "f":
+    elif test_type.find("f") != -1:
         filename = input("Enter file name: ")
         with open(f"tests/{filename}", 'r') as f:
             n = int(f.readline())
             data = list(map(int, f.readline().split()))
     else:
-        raise ValueError("Invalid input type. Expected I or F.")
+        raise Exception("Unknown command")
 
-    swaps = build_heap(data)
+    assert len(data) == n
 
+    swaps = build_heap(data, n)
+    
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
